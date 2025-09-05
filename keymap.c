797cfb78f271364b4fd8bb21b206b2f9ba129792
rhusiev <63148080+rhusiev@users.noreply.github.com>
2025-09-05 16:27:11 +0300

    Move in GAME KC_1 to pinky up, LALT to left, LCTL to down left

--- keymap.c
+++ keymap.c
@@ -337,9 +337,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     KC_LGUI, KC_RSFT, TD(LCTL_LALT), /**/ KC_LBRC, KC_SPC, OSL(L_SYMBOL)
     ),
     [L_GAME] = LAYOUT_split_3x6_3(
-        KC_ESC,  KC_LALT, KC_Q, KC_W,   KC_E,    KC_R,        /**/ KC_T,    KC_Y,           KC_U,           KC_I,    KC_O,   KC_P,
-        KC_LCTL, KC_2,    KC_A, KC_S,   KC_D,    KC_F,        /**/ KC_G,    KC_H,           KC_J,           KC_K,    KC_L,   KC_SCLN,
-        KC_1,    KC_3,    KC_T, KC_G,   KC_B,    KC_C,        /**/ KC_B,    KC_N,           KC_M,           KC_COMM, KC_DOT, KC_SLSH,
+        KC_ESC,  KC_1, KC_Q, KC_W,   KC_E,    KC_R,        /**/ KC_T,    KC_Y,           KC_U,           KC_I,    KC_O,   KC_P,
+        KC_LALT, KC_2, KC_A, KC_S,   KC_D,    KC_F,        /**/ KC_G,    KC_H,           KC_J,           KC_K,    KC_L,   KC_SCLN,
+        KC_LCTL, KC_3, KC_T, KC_G,   KC_B,    KC_C,        /**/ KC_B,    KC_N,           KC_M,           KC_COMM, KC_DOT, KC_SLSH,
                              KC_SPC, KC_LSFT, MO(L_GAME1), /**/ KC_LBRC, KC_SPC, OSL(L_SYMBOL)
     ),
     [L_GAME1] = LAYOUT_split_3x6_3(
